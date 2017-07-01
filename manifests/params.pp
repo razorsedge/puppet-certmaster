c4f16605dd9e612db4a27a265a38a2dc14759336
Michael Arnold <github@razorsedge.org>
2017-07-01 00:23:59 -0400

    Support STRICT_VARIABLES for Puppet 4.

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,19 +17,25 @@ class certmaster::params {
 
 ### The following parameters should not need to be changed.
 
-  $listen_addr = $::certmaster_listen_addr ? {
-    undef   => '',
-    default => $::certmaster_listen_addr,
+  $certmaster_listen_addr = getvar('::certmaster_listen_addr')
+  if $certmaster_listen_addr {
+    $listen_addr = $::certmaster_listen_addr
+  } else {
+    $listen_addr = '' # lint:ignore:empty_string_assignment
   }
 
-  $certmaster = $::certmaster_certmaster ? {
-    undef   => 'certmaster',
-    default => $::certmaster_certmaster,
+  $certmaster_certmaster = getvar('::certmaster_certmaster')
+  if $certmaster_certmaster {
+    $certmaster = $::certmaster_certmaster
+  } else {
+    $certmaster = 'certmaster'
   }
 
-  $autosign = $::certmaster_autosign ? {
-    undef   => false,
-    default => $::certmaster_autosign,
+  $certmaster_autosign = getvar('::certmaster_autosign')
+  if $certmaster_autosign {
+    $autosign = $::certmaster_autosign
+  } else {
+    $autosign = false
   }
   if is_string($autosign) {
     $safe_autosign = str2bool($autosign)
@@ -37,9 +43,11 @@ class certmaster::params {
     $safe_autosign = $autosign
   }
 
-  $use_puppet_certs = $::func_use_puppet_certs ? {
-    undef   => false,
-    default => $::func_use_puppet_certs,
+  $func_use_puppet_certs = getvar('::func_use_puppet_certs')
+  if $func_use_puppet_certs {
+    $use_puppet_certs = $::func_use_puppet_certs
+  } else {
+    $use_puppet_certs = false
   }
   if is_string($use_puppet_certs) {
     $safe_use_puppet_certs = str2bool($use_puppet_certs)
@@ -51,21 +59,27 @@ class certmaster::params {
 
   # If we have a top scope variable defined, use it, otherwise fall back to a
   # hardcoded value.
-  $ensure = $::certmaster_ensure ? {
-    undef   => 'present',
-    default => $::certmaster_ensure,
+  $certmaster_ensure = getvar('::certmaster_ensure')
+  if $certmaster_ensure {
+    $ensure = $::certmaster_ensure
+  } else {
+    $ensure = 'present'
   }
 
-  $service_ensure = $::certmaster_service_ensure ? {
-    undef   => 'stopped',
-    default => $::certmaster_service_ensure,
+  $certmaster_service_ensure = getvar('::certmaster_service_ensure')
+  if $certmaster_service_ensure {
+    $service_ensure = $::certmaster_service_ensure
+  } else {
+    $service_ensure = 'stopped'
   }
 
   # Since the top scope variable could be a string (if from an ENC), we might
   # need to convert it to a boolean.
-  $autoupgrade = $::certmaster_autoupgrade ? {
-    undef   => false,
-    default => $::certmaster_autoupgrade,
+  $certmaster_autoupgrade = getvar('::certmaster_autoupgrade')
+  if $certmaster_autoupgrade {
+    $autoupgrade = $::certmaster_autoupgrade
+  } else {
+    $autoupgrade = false
   }
   if is_string($autoupgrade) {
     $safe_autoupgrade = str2bool($autoupgrade)
@@ -73,9 +87,11 @@ class certmaster::params {
     $safe_autoupgrade = $autoupgrade
   }
 
-  $service_enable = $::certmaster_service_enable ? {
-    undef   => false,
-    default => $::certmaster_service_enable,
+  $certmaster_service_enable = getvar('::certmaster_service_enable')
+  if $certmaster_service_enable {
+    $service_enable = $::certmaster_service_enable
+  } else {
+    $service_enable = false
   }
   if is_string($service_enable) {
     $safe_service_enable = str2bool($service_enable)
@@ -83,9 +99,11 @@ class certmaster::params {
     $safe_service_enable = $service_enable
   }
 
-  $service_hasrestart = $::certmaster_service_hasrestart ? {
-    undef   => true,
-    default => $::certmaster_service_hasrestart,
+  $certmaster_service_hasrestart = getvar('::certmaster_service_hasrestart')
+  if $certmaster_service_hasrestart {
+    $service_hasrestart = $::certmaster_service_hasrestart
+  } else {
+    $service_hasrestart = true
   }
   if is_string($service_hasrestart) {
     $safe_service_hasrestart = str2bool($service_hasrestart)
@@ -94,9 +112,11 @@ class certmaster::params {
   }
 
   # RHEL certmaster init script does not have a functioning status
-  $service_hasstatus = $::certmaster_service_hasstatus ? {
-    undef   => false,
-    default => $::certmaster_service_hasstatus,
+  $certmaster_service_hasstatus = getvar('::certmaster_service_hasstatus')
+  if $certmaster_service_hasstatus {
+    $service_hasstatus = $::certmaster_service_hasstatus
+  } else {
+    $service_hasstatus = false
   }
   if is_string($service_hasstatus) {
     $safe_service_hasstatus = str2bool($service_hasstatus)
